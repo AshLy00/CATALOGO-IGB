91128751b6fea28eb9437c1c1477b8c193801283
AshLy00 <140760135+AshLy00@users.noreply.github.com>
2024-01-22 15:38:45 -0500

    Update TheFooter.vue

--- src/components/templates/TheFooter.vue
+++ src/components/templates/TheFooter.vue
@@ -1,9 +1,25 @@
-<script setup></script>
+<script setup>
+const promptAddToHomeScreen = () => {
+  if (window.matchMedia("(display-mode: standalone)").matches) {
+    // La aplicación ya está instalada en la pantalla de inicio
+    console.log("La aplicación ya está instalada en la pantalla de inicio.");
+  } else if (window.navigator.standalone === true) {
+    // iOS Safari
+    console.log("La aplicación ya está instalada en la pantalla de inicio.");
+  } else if (window.matchMedia("(display-mode: browser)").matches) {
+    // Mostrar mensaje o instrucciones para agregar a la pantalla de inicio
+    console.log("Mostrar instrucciones para agregar a la pantalla de inicio.");
+  }
+};
+</script>
 <template>
   <footer>
     <div class="footer_container">
       <p>copyright © 2024 - inversiones gomez barraza</p>
     </div>
+    <button @click="promptAddToHomeScreen">
+      Agregar a la pantalla de inicio
+    </button>
   </footer>
 </template>
 
